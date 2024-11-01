2c53a9c74411532244f2d6350ff517ec8442935b
maruf_hossain <marufhossain123098@gmail.com>
2024-11-01 22:28:39 +0600

    added vedio uploading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,39 @@
-import Home from './components/Home'
-import './App.css'
+import React from 'react'
+import VideoPlayer from './components/VedioPlayer'
 
 function App() {
+  const playerRef = React.useRef(null);
 
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [{
+      src: '/path/to/video.mp4',
+      type: 'video/mp4'
+    }]
+  };
+
+  const handlePlayerReady = (player) => {
+    playerRef.current = player;
+
+    // You can handle player events here, for example:
+    player.on('waiting', () => {
+     VideoPlayer.log('player is waiting');
+    });
+
+    player.on('dispose', () => {
+      VideoPlayer.log('player will dispose');
+    });
+  };
 
   return (
-    <>
-        <Home></Home>
-    </>
+      <>
+     <div>Rest of app here</div>
+      <VideoPlayer options={videoJsOptions} onReady={handlePlayerReady} />
+      <div>Rest of app here</div>
+      </>
   )
 }
 
