074685ba56a5f9b052abf3a588891ebe9380899b
maruf_hossain <marufhossain123098@gmail.com>
2024-11-08 22:54:56 +0600

    server is updated

--- server/routes/router.js
+++ server/routes/router.js
@@ -1,10 +1,8 @@
 import express from "express";
 import { fileUpload } from "../modules/module.js";
 import upload from "../uploadOPeration/upload.js";
-
-
+import { vedioUpload } from "../modules/module.js";
 const router = express.Router();
-
 router.post("/upload", upload.single("vedio"),fileUpload);
-
+router.post("/vedioUpload", upload.single("vedio"),vedioUpload);
 export default router;
